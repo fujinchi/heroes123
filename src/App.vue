215fe14dac270eba93663ee695ca842f0761ae2c
fu <fujinchi123@126.com>
2018-08-09 00:02:54 +0800

    AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,10 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">CRUD</a>
-        </div>
-      </div>
-    </nav>
+    <!-- 网页的 header -->
+    <app-header></app-header>
     <div class="container-fluid">
       <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Reports</a></li>
-            <li><a href="#">Analytics</a></li>
-            <li><a href="#">Export</a></li>
-          </ul>
-        </div>
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-        <h2 class="sub-header">Hero List</h2>
+        <!-- 侧边导航栏 -->
         <a class="btn btn-success" href="add.html">Add</a>
         <div class="table-responsive">
           <table class="table table-striped">
@@ -241,15 +221,16 @@
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  // 导入 AppHeader.vue 头部组件
+  import AppHeader from './components/AppHeader.vue';
+  // ES6导入模块的语法
+  export default {
+    // 局部组件
+    components: {
+      AppHeader
     }
-  }
-}
+  };
 </script>
-
+  
 <style>
 </style>
